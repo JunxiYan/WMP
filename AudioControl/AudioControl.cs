aaa0c39a345048f94ff22481982cdceccfbecca3
Junxi Yan <junxi.yan@hotmail.com>
2024-05-10 23:32:55 +0800

    Add project files.

---
+++ AudioControl/AudioControl.cs
@@ -0,0 +1,24 @@
+﻿using NAudio.Wave;
+namespace AudioControl
+{
+    public class AudioControl
+    {
+        public static async Task Play(string[] args)
+        {
+            var audioFile = new AudioFileReader("D:\\microsoft\\source\\repos\\WMP\\WMP\\sample.mp3");
+            var outputDevice = new WaveOutEvent();
+            outputDevice.Init(audioFile);
+            outputDevice.Play();
+
+            await Task.Run(() =>
+            {
+                while (outputDevice.PlaybackState == PlaybackState.Playing)
+                {
+                    Task.Delay(1000).Wait();
+                }
+            });
+
+
+        }
+    }
+}
